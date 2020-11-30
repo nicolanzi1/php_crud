1330d9e6b97b6fc4e0b9ba56a6d23737dd231dd2
nicolanzi1 <nicolanzi@gmail.com>
2020-11-30 15:42:31 +0000

    Connected index.php to the database inserting the Name and Location records into the data table if the Save button is clicked

--- index.php
+++ index.php
@@ -10,20 +10,57 @@
 </head>
 <body>
     <?php require_once 'process.php'; ?>
-    <div class="row justify-content-center">
-        <form action="process.php" method="post">
-            <div class="form-group">
-                <label>Name</label>
-                <input type="text" name="name" class="form-control" placeholder="Enter your name">
+        <div class="container">
+        <?php
+            $mysqli = new mysqli('localhost', 'root', 'myphppassword', 'crud') or die(mysqli_error($mysqli));
+            $result = $mysqli->query("SELECT * FROM data");
+            // pre_r($result);
+            ?>
+
+            <div class="row justify-content-center">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Location</th>
+                            <th colspan=2>Action</th>
+                        </tr>
+                    </thead>
+
+                    <?php
+                        while ($row = $result->fetch_assoc()): ?>
+                            <tr>
+                                <td><?php echo $row['name']; ?></td>
+                                <td><?php echo $row['location']; ?></td>
+                                <td></td>
+                            </tr>
+                        <?php endwhile; ?>
+                </table>
             </div>
-            <div class="form-group">
-                <label>Location</label>
-                <input type="text" name="location" class="form-control" placeholder="Enter your location">
-            </div>
-            <div class="form-group">
-                <button type="submit" class="btn btn-primary" name="save">Save</button>
-            </div>
-        </form>
+            <?php
+
+            function pre_r( $array ) {
+                echo '<pre>';
+                    print_r($array);
+                echo '</pre>';
+            }
+        ?>
+
+        <div class="row justify-content-center">
+            <form action="process.php" method="post">
+                <div class="form-group">
+                    <label>Name</label>
+                    <input type="text" name="name" class="form-control" placeholder="Enter your name">
+                </div>
+                <div class="form-group">
+                    <label>Location</label>
+                    <input type="text" name="location" class="form-control" placeholder="Enter your location">
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary" name="save">Save</button>
+                </div>
+            </form>
+        </div>
     </div>
 </body>
 </html>
