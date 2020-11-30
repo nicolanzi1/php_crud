c170a02cf27d04a5d6e9f1aef375bc6428a919ba
nicolanzi1 <nicolanzi@gmail.com>
2020-11-30 14:41:27 +0000

    Added jQuery and Bootstrap CDN, div tags and classes to the form, name and location inputs, as well as the save button, to style them

--- index.php
+++ index.php
@@ -3,15 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <title>PHP Crud</title>
 </head>
 <body>
-    <form action="" method="post">
-        <label>Name</label>
-        <input type="text" name="name" value="Enter your name">
-        <label>Location</label>
-        <input type="text" name="location" value="Enter your location">
-        <button type="submit" name="save">Save</button>
-    </form>
+    <div class="row justify-content-center">
+        <form action="" method="post">
+            <div class="form-group">
+                <label>Name</label>
+                <input type="text" name="name" class="form-control" value="Enter your name">
+            </div>
+            <div class="form-group">
+                <label>Location</label>
+                <input type="text" name="location" class="form-control" value="Enter your location">
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary" name="save">Save</button>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
