07a442683ef04787526c5784249f7e59267b682c
nicolanzi1 <nicolanzi@gmail.com>
2020-11-30 14:11:04 +0000

    Wrote an index.php containing a form with input labels and fields for name and location, and a Save submit button

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Crud</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label>Name</label>
+        <input type="text" name="name" value="Enter your name">
+        <label>Location</label>
+        <input type="text" name="location" value="Enter your location">
+        <button type="submit" name="save">Save</button>
+    </form>
+</body>
+</html>
