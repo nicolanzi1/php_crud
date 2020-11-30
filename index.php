455ef8698a76a3281471d1a85bc37eacf9427602
nicolanzi1 <nicolanzi@gmail.com>
2020-11-30 16:02:23 +0000

    Created session messages and message types for the save and delete buttons when clicked, and added header function to redirect users to index.php

--- index.php
+++ index.php
@@ -10,6 +10,20 @@
 </head>
 <body>
     <?php require_once 'process.php'; ?>
+
+    <?php
+
+        if (isset($_SESSION['message'])): ?>
+
+        <div class="alert alert-<?=$_SESSION['msg_type']?>">
+
+            <?php
+                echo $_SESSION['message'];
+                unset($_SESSION['message']);
+            ?>
+        </div>
+    <?php endif ?>
+
         <div class="container">
         <?php
             $mysqli = new mysqli('localhost', 'root', 'myphppassword', 'crud') or die(mysqli_error($mysqli));
--- process.php
+++ process.php
@@ -1,5 +1,7 @@
 <?php
 
+session_start();
+
 $mysqli = new mysqli('localhost', 'root', 'myphppassword', 'crud') or die(mysqli_error($mysqli));
 
 if (isset($_POST['save'])){
@@ -7,11 +9,21 @@ if (isset($_POST['save'])){
     $location = $_POST['location'];
 
     $mysqli->query("INSERT INTO data (name, location) VALUES('$name', '$location')") or die($mysqli->error);
+
+    $_SESSION['message'] = "Record has been saved!";
+    $_SESSION['msg_type'] = "success";
+
+    header("location: index.php");
 }
 
 if (isset($_GET['delete'])){
     $id = $_GET['delete'];
     $mysqli->query("DELETE FROM data WHERE id=$id") or die($mysql->error());
+
+    $_SESSION['message'] = "Record has been deleted!";
+    $_SESSION['msg_type'] = "danger";
+
+    header("location: index.php");
 }
 
 ?>
